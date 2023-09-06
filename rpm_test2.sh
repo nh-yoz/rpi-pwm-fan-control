c34babe671f0857e4c67d201c75faef5bf9507e0
Niklas Hook <82522085+nh-yoz@users.noreply.github.com>
2023-09-06 15:56:10 +0200

    Update rpm_test2.sh

--- rpm_test2.sh
+++ rpm_test2.sh
@@ -18,7 +18,6 @@
 GPIO=24 # The GPIO on which the fan's tachymeter is connected
 SAMPLE_TIME="0.2" # The time to acquire information from the tachymeter
 PULSES_PER_REVOLUTION=2 # The number of pulses that are emitted per revolution of the fan
-HANDLE=$(pigs no) # Get a handle for a new notification from pigs
 BIT=$((1 << $GPIO)) # Create the bit-mask for the gpio. Gpio 5 -> 100000
 
 # Get the caracter used by pig2vcd for the gpio (gpio 0-25 is A-Z, 26-51 is a-z
@@ -28,25 +27,6 @@ then
 else
     GPIO_CHAR=$(printf "\\$(printf '%03o' $((65+6+$GPIO)))")
 fi
-
-# Start notification, keep running for $SAMPLE_TIME seconds then pause de notification
-pigs nb $HANDLE $BIT && sleep $SAMPLE_TIME && pigs np $HANDLE
-
-# Get a temporary file name
-TMP=$(mktemp)
-
-# Convert notification to readable using the command "pig2vcd", remove header and "#" in front of microseconds and write to temp-file
-# The notification file is a pipe -> put a timeout
-timeout 0.2 cat /dev/pigpio$HANDLE | pig2vcd | sed "0,/0$GPIO_CHAR/d" | sed 's/[ #]//' > $TMP 
-
-# Close the notification
-pigs nc $HANDLE
-
-# Read the content of the temp-file to an array
-LINES=()
-while IFS= read -r LINE; do
-    LINES+=("$LINE")
-done < $TMP
 
 FindTimeDiff() { # Read the file and print to stdout the timediff between two occurences of "1$GPIO_CHAR". If none, print 0.
     RES=0
@@ -73,16 +53,41 @@ FindTimeDiff() { # Read the file and print to stdout the timediff between two oc
     echo $RES
 }
 
-RESULT=$(FindTimeDiff)
-if [ "$RESULT" == "0" ]
-then 
-    # No state change -> problem or fan stalled
-    echo "0 RPM"
-else 
-    RESULT=$(echo "scale=10;1/($RESULT/1000000)*60/$PULSES_PER_REVOLUTION" | bc) # get the rpm (with decimals)
-    RESULT=$(echo "$RESULT/1" | bc) # Truncate value
-    echo "$RESULT RPM"
-fi
+while :
+    HANDLE=$(pigs no) # Get a handle for a new notification from pigs
 
-# Delete temp-file
-rm -f $TMP
+    # Start notification, keep running for $SAMPLE_TIME seconds then pause de notification
+    pigs nb $HANDLE $BIT && sleep $SAMPLE_TIME && pigs np $HANDLE
+
+    # Get a temporary file name
+    TMP=$(mktemp)
+
+    # Convert notification to readable using the command "pig2vcd", remove header and "#" in front of microseconds and write to temp-file
+    # The notification file is a pipe -> put a timeout
+    timeout 0.2 cat /dev/pigpio$HANDLE | pig2vcd | sed "0,/0$GPIO_CHAR/d" | sed 's/[ #]//' > $TMP 
+
+    # Close the notification
+    pigs nc $HANDLE
+
+    # Read the content of the temp-file to an array
+    LINES=()
+    while IFS= read -r LINE; do
+        LINES+=("$LINE")
+    done < $TMP
+
+    RESULT=$(FindTimeDiff)
+    if [ "$RESULT" == "0" ]
+    then 
+        # No state change -> problem or fan stalled
+        echo "0 RPM"
+    else 
+        RESULT=$(echo "scale=10;1/($RESULT/1000000)*60/$PULSES_PER_REVOLUTION" | bc) # get the rpm (with decimals)
+        RESULT=$(echo "$RESULT/1" | bc) # Truncate value
+        echo "$RESULT RPM"
+    fi
+
+    # Delete temp-file
+    rm -f $TMP
+    [ ! -v $1 ] && break
+    sleep $1
+done
